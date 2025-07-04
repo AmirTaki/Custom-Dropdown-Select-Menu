3201258b583370d9837d39bada981c71389f9cf0
amir <amirtaki623@gmail.com>
2025-07-04 23:53:35 +0330

    option js

--- src/main.js
+++ src/main.js
@@ -7,5 +7,10 @@ const sBtn_text = optionMenu.querySelector(".sBtn-text")
 selectBtn.addEventListener('click', ()=>{
     optionMenu.classList.toggle('active')
 
-    
+    options.forEach(option => {
+        option.addEventListener('click', ()=> {
+            let selectOption = option.querySelector('.option-text').innerText;
+            sBtn_text.innerText = selectOption
+        })
+    })
 })

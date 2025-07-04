a548eeebb19791aa03bc7dca00c9e1cbea29697c
amir <amirtaki623@gmail.com>
2025-07-04 23:51:12 +0330

    querySelect js

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const optionMenu = document.querySelector('.select-menu')
+const selectBtn = optionMenu.querySelector('.select-btn')
+const options = optionMenu.querySelectorAll('.option')
+const sBtn_text = optionMenu.querySelector(".sBtn-text")
